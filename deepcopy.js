eacac01a33b90e05d6adc3d0fc3bba9bb0cc8719
mregydev <mregydev@gmail.com>
2017-05-13 10:20:42 -0700

    Update deepcopy.js

--- deepcopy.js
+++ deepcopy.js
@@ -2,10 +2,10 @@
 ///*****Main Concepts used Here are (Arrow functions - Destructring - Recusrion - block scoped declaration with let - foreach looping) *****///
 ///**********///
 
-//return object which is deep copied from the passed source object
+
 function DeepCopy(srcObj) {
     //In case source object is primitive return
-    if (typeof (srcObj) != "object")
+    if (typeof srcObj != "object")
         return null;
     let clonedObject = {};
     //extract source object properites
@@ -18,7 +18,10 @@ function DeepCopy(srcObj) {
 }
 
 
+
+
 //Example 
+
 var firstStudent = { firstName: "alaa", major: "computer science", university: { name: "AinShams", location: "Cairo" } },
     secondStudent = { firstName: "yehia", major: "computer science", university: { name: "Helwan", location: "Helwan" } },
     //Cloning two objects (firstStudent,secondStudent) and assign them using destructring
